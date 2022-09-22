8fe64e8dd9c9bdd2407807944981ec101c3102c3
Roman <tsugelroman1998@gmail.com>
2022-09-23 01:39:32 +0300

    first commit

---
+++ proj.js
@@ -0,0 +1,16 @@
+
+const mas = [1,2,3,4,5,6]
+let n = mas.length;
+
+for(let i=0; i < n; i++)
+{
+    console.log(mas[i]);
+}
+
+    console.log('-----------');
+
+for(let i=n-1; i >= 0; i--)
+{
+    console.log(mas[i]);
+}
+    
